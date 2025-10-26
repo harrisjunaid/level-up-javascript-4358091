8a58833ef0e7f32afe99f30a8fcadf072bef66a4
Harris Junaid <harris.junaid@gmail.com>
2025-10-26 19:42:28 +0000

    devcontainer terminal to $

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 git config --global user.name "Harris Junaid"
 git config --global user.email "harris.junaid@gmail.com"
+# Create a basic .bashrc file if it doesn't exist
+if [ ! -f ~/.bashrc ]; then
+  echo "# .bashrc created by setup.sh" > ~/.bashrc # Create a new .bashrc file
+fi
+
+echo 'PS1="$ "' >> ~/.bashrc
+
